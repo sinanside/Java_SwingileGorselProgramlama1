0bba0df810a71fac389d1c95f684a4aa27f43b4e
Engin Dalga <mackaengin@gmail.com>
2025-03-28 21:13:11 +0300

    ilk swing örneğimiz güncel

--- src/App.java
+++ src/App.java
@@ -1,14 +1,24 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App extends JFrame {
-    private JButton button1;
+    private JButton gönderButton;
+    private JPanel Jpanel;
 
     public App() {
+        setContentPane(Jpanel);
         setTitle("İlk Java Formumuz");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(400,400);
         setLocationRelativeTo(null);
         setVisible(true);
+        gönderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(App.this,"Butona tıkladınız...");
+            }
+        });
     }
 
     public static void main(String[] args) {
